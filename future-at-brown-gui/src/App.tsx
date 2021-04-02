6a3d232cf245cce137c8a5d2aa0f6e3bf5abbe96
Raymond Dai <raymond_dai@brown.edu>
2021-04-02 02:24:30 -0400

    fully installed typescript

---
+++ future-at-brown-gui/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello! </h1>
+    </div>
+  );
+}
+
+export default App;
