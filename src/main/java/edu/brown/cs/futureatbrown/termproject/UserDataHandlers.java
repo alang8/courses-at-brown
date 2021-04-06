f609abe3665bf9490e184c1f07d6d57413f18d9a
Filip Kierzenka <filip@Filips-MacBook-Pro.local>
2021-04-06 00:27:54 -0400

    login stuff resistant to sql injection

--- src/main/java/edu/brown/cs/futureatbrown/termproject/UserDataHandlers.java
+++ src/main/java/edu/brown/cs/futureatbrown/termproject/UserDataHandlers.java
@@ -36,55 +36,56 @@ public class UserDataHandlers {
     @Override
     public Object handle(Request request, Response response) {
       Map<String, Object> variables;
+      String msg;
+      Map<String, Double> presets = new HashMap<>();
       try {
         JSONObject data = new JSONObject(request.body());
 
         String inputtedUsername = data.getString("username");
         String inputtedPassword = data.getString("password");
 
-        String alphanumRegex = "^[a-zA-Z0-9_]+$";
-
-
-
-
-        Base64.Encoder coder = Base64.getEncoder();
-        String hashedUsername = coder.encodeToString(inputtedUsername.getBytes());
-        String hashedPassword = coder.encodeToString(inputtedPassword.getBytes());
-
-        String query = "SELECT * FROM user_data WHERE username = ?;";
-        PreparedStatement prep = conn.prepareStatement(query);
-        prep.setString(1, hashedUsername);
-        ResultSet rs = prep.executeQuery();
-        String msg;
-        Map<String, Double> presets = new HashMap<>();
-        double crsRatingPref = 2.5;
-        double avgHoursPref = 10.0;
-        double maxHoursPref = 12.0;
-        double crsSizePref = 20.0;
-        double profRatingPref = 2.5;
-        presets.put("crsRatingPref", crsRatingPref);
-        presets.put("avgHoursPref", avgHoursPref);
-        presets.put("maxHoursPref", maxHoursPref);
-        presets.put("crsSizePref", crsSizePref);
-        presets.put("profRatingPref", profRatingPref);
-
-        if (rs.next()) {
-          //found a match
-          msg = "Username already taken!";
+        String alphanumRegex = "/^[a-z0-9]+$/i";
+        if (!inputtedUsername.matches(alphanumRegex)) {
+          msg = "Username must contain alphanumeric characters only!";
+        } else if (inputtedPassword.contains("\"") || inputtedPassword.contains("'")) {
+          msg = "Password cannot contain single/double quotes!";
         } else {
-          msg = "Account Added!";
-          query = "INSERT INTO user_data VALUES (?, ?, ?, ?, ?, ?, ?);";
-          prep = conn.prepareStatement(query);
-          prep.setString(1, hashedUsername);
-          prep.setString(2, hashedPassword);
-          prep.setDouble(3, crsRatingPref);
-          prep.setDouble(4, avgHoursPref);
-          prep.setDouble(5, maxHoursPref);
-          prep.setDouble(6, crsSizePref);
-          prep.setDouble(7, profRatingPref);
-          prep.executeQuery();
-        }
+          Base64.Encoder coder = Base64.getEncoder();
+          String hashedUsername = coder.encodeToString(inputtedUsername.getBytes());
+          String hashedPassword = coder.encodeToString(inputtedPassword.getBytes());
 
+          String query = "SELECT * FROM user_data WHERE username = ?;";
+          PreparedStatement prep = conn.prepareStatement(query);
+          prep.setString(1, hashedUsername);
+          ResultSet rs = prep.executeQuery();
+          double crsRatingPref = 2.5;
+          double avgHoursPref = 10.0;
+          double maxHoursPref = 12.0;
+          double crsSizePref = 20.0;
+          double profRatingPref = 2.5;
+          presets.put("crsRatingPref", crsRatingPref);
+          presets.put("avgHoursPref", avgHoursPref);
+          presets.put("maxHoursPref", maxHoursPref);
+          presets.put("crsSizePref", crsSizePref);
+          presets.put("profRatingPref", profRatingPref);
+
+          if (rs.next()) {
+            //found a match
+            msg = "Username already taken!";
+          } else {
+            msg = "Account Added!";
+            query = "INSERT INTO user_data VALUES (?, ?, ?, ?, ?, ?, ?);";
+            prep = conn.prepareStatement(query);
+            prep.setString(1, hashedUsername);
+            prep.setString(2, hashedPassword);
+            prep.setDouble(3, crsRatingPref);
+            prep.setDouble(4, avgHoursPref);
+            prep.setDouble(5, maxHoursPref);
+            prep.setDouble(6, crsSizePref);
+            prep.setDouble(7, profRatingPref);
+            prep.executeQuery();
+          }
+        }
         variables = ImmutableMap.of("message", msg, "presets", presets);
       } catch (JSONException | SQLException e) {
         variables = ImmutableMap.of("message", "ERROR: SignUpHandler", "presets", null);
