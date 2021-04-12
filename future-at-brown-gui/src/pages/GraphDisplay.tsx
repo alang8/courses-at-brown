1c6f576d67d24033c82d700a1ca71046cafa0f54
Filip Kierzenka <filip@filips-mbp.devices.brown.edu>
2021-04-12 17:27:03 -0400

    search results almost hooked up to /graph

--- future-at-brown-gui/src/pages/GraphDisplay.tsx
+++ future-at-brown-gui/src/pages/GraphDisplay.tsx
@@ -6,10 +6,19 @@ import "../css/Graph.css"
 import { AuthenticatedPageProps } from "../classes/Authentication";
 import SignOutHeader from "../modules/SignOutHeader";
 import { ProfileButton, SearchButton } from "../modules/BottomButton";
+import {Course} from "../classes/Course";
+import User from "../classes/User";
 
-const GraphDisplay: React.FC<AuthenticatedPageProps> = (props) => {
+interface Params {
+    user: User;
+    setUser: (user: User | undefined) => void;
+    path : {[code:string]:number};
+}
 
-    const samplePath = { "CSCI 0170": 0, "CSCI 0220": 1, "CSCI 0180": 1, "CSCI 0330": 2, "CSCI 1470": 2, "CSCI 0320": 3, "APMA 0360": 5 };
+
+const GraphDisplay: React.FC<Params> = (props) => {
+
+    // const samplePath = { "CSCI 0170": 0, "CSCI 0220": 1, "CSCI 0180": 1, "CSCI 0330": 2, "CSCI 1470": 2, "CSCI 0320": 3, "APMA 0360": 5 };
 
     return <div className="total">
         <ProfileButton />
@@ -17,7 +26,7 @@ const GraphDisplay: React.FC<AuthenticatedPageProps> = (props) => {
         <SignOutHeader setUser={props.setUser} user={props.user} heading={"Graph"}/>
 
         <div style={{ height: '100vh' }}>
-            <ClassGraph path={samplePath} />
+            <ClassGraph path={props.path} />
         </div>
 
     </div>
